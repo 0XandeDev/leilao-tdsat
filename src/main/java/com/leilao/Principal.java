9633fab43f41b683d8cc4d8617ae865415536e24
0XandeDev <alexandre.mach14@gmail.com>
2025-11-19 16:16:33 -0300

    Define cadastroVIEW como tela principal do sistema

---
+++ src/main/java/com/leilao/Principal.java
@@ -0,0 +1,10 @@
+package com.leilao;
+
+import com.leilao.view.cadastroVIEW;
+
+public class Principal {
+    public static void main(String[] args) {
+        // Define cadastroVIEW como tela principal
+        new cadastroVIEW().setVisible(true);
+    }
+}
